f19bf3071670d3da423d09b6b30870d752cd2593
pragatimehrotra <mehrotra.pragati0@gmail.com>
2019-07-30 15:24:36 +0530

    Minor fix

--- API/src/main/java/com/carparking/api/Repository/HistoryRepository.java
+++ API/src/main/java/com/carparking/api/Repository/HistoryRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.Repository;
 import java.util.List;
 
 public interface HistoryRepository extends Repository<History, Integer> {
-    
+
     List<History> findHistoryByUserId(Integer userId);
 }
